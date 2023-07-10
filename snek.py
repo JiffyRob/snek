9d6ac69b43725b584cffb29d826868d86f16b7ba
John Robinson <jfrobinson767@gmail.com>
2023-07-10 12:03:38 -0500

    rewrote to a shlex based parser and javascript like syntax

--- snek.py
+++ snek.py
@@ -1,68 +1,11 @@
 import functools
 import operator
+import random
+import shlex
 import time
-from dataclasses import dataclass
-from typing import Any
 
-
-class Argtype:
-    INT = 0
-    STR = 1
-    DATA = 2
-
-
-class SNEKConst:
-    UNFINISHED = "const UNFINISHED"
-    NULL = "const NULL"
-    END = "const END"
-    SET_COMMAND = "SET"
-    EQ_OPERATOR = "="
-
-
-@dataclass
-class Token:
-    data: Any
-
-    def convert(self):
-        return self.data
-
-    def __str__(self):
-        return str(self.data)
-
-
-@dataclass
-class EQToken(Token):
-    pass
-
-
-@dataclass
-class CommandToken(Token):
-    pass
-
-
-@dataclass
-class NoneToken(Token):
-    pass
-
-
-@dataclass
-class ArgToken(Token):
-    pass
-
-
-@dataclass
-class KwdToken(Token):
-    pass
-
-
-@dataclass
-class VarnameToken(Token):
-    pass
-
-
-@dataclass
-class SetToken(Token):
-    pass
+UNFINISHED = "const unfinished"
+NEXT = "const next"
 
 
 class SnekError(SyntaxError):
@@ -86,7 +29,7 @@ class Wait(SnekCommand):
         now = time.time() * 1000
         if now - self.start >= self.time:
             return 1
-        return SNEKConst.UNFINISHED
+        return UNFINISHED
 
 
 def snek_command(func):
@@ -98,203 +41,217 @@ def snek_command(func):
 
 
 def unfinished_run():
-    yield SNEKConst.UNFINISHED
+    yield UNFINISHED
     yield 1
 
 
-class SnekProgram:
+def neq(*args):
+    last_arg = args[0]
+    for arg in args:
+        if arg != last_arg:
+            return True
+    return False
+
+
+class SNEKProgram:
     def __init__(self, script, start_variables=None, api=None):
         self.api = {
-            "eq": snek_command(lambda *args: functools.reduce(operator.eq, args)),
-            "neq": snek_command(lambda *args: not functools.reduce(operator.eq, args)),
-            "add": snek_command(lambda *args: functools.reduce(operator.add, args)),
-            "sub": snek_command(lambda *args: functools.reduce(operator.sub, args)),
-            "mult": snek_command(lambda *args: functools.reduce(operator.mul, args)),
-            "div": snek_command(
-                lambda *args: functools.reduce(operator.floordiv, args)
-            ),
+            "upper": snek_command(lambda arg: arg.upper()),
+            "lower": snek_command(lambda arg: arg.lower()),
+            "title": snek_command(lambda arg: arg.title()),
             "print": self.print,
             "wait": Wait,
+            "randint": random.randint,
+            "input": snek_command(input),
+            "not": snek_command(lambda arg: operator.not_(arg)),
+            "bool": snek_command(lambda arg: bool(arg)),
+            "contains": snek_command(lambda seq, sub: operator.contains(seq, sub)),
+            "lt": snek_command(lambda a, b: a < b),
+            "le": snek_command(lambda a, b: a <= b),
+            "eq": snek_command(lambda *args: functools.reduce(operator.ne, args)),
+            "neq": snek_command(neq),
+            "ge": snek_command(lambda a, b: a >= b),
+            "gt": snek_command(lambda a, b: a > b),
+            "abs": snek_command(abs),
+            "add": snek_command(lambda *args: sum(args)),
+            "mult": snek_command(lambda *args: functools.reduce(operator.mul, args)),
+            "neg": snek_command(lambda a: -a),
+            "pow": snek_command(lambda a, b: a**b),
+            "sub": snek_command(lambda a, *args: a - sum(args)),
+            "div": snek_command(lambda *args: functools.reduce(operator.truediv, args)),
+            "fdiv": snek_command(
+                lambda *args: functools.reduce(operator.floordiv, args)
+            ),
+            "inv": snek_command(operator.inv),
+            "lshift": snek_command(operator.lshift),
+            "rshift": snek_command(operator.rshift),
+            "xor": snek_command(operator.xor),
+            "and": snek_command(operator.and_),
+            "or": snek_command(operator.or_),
         }
         if api is not None:
             self.api.update(api)
         self.kwd = {
-            "IF": self._if,
-            "ENDIF": self._endif,
-            "SWITCH": self._switch,
-            "ENDSWITCH": self._endswitch,
-            "CASE": self._case,
-            "ENDCASE": self._endcase,
-            "BREAK": unfinished_run,
+            "if": self._if,
+            "switch": self._switch,
+            "case": self._case,
+            "}": self._end,
         }
-        # dict of variables
+        # variables go here
         self.namespace = {}
         if start_variables is not None:
             self.namespace.update(start_variables)
-        # list of bools from evaluated if statements
-        self.conditions = [True]
-        # list of switch var names
-        self.switches = []
-        # list of cases in switch statements
-        self.cases = [True]
-        # name to set next command to
+        # actual lexing
+        self.lexer = shlex.shlex(script, punctuation_chars="()+-*/=")
+        # interpreter state and control flow
+        self.running = True
+        self.current_command = None
+        self.control_statements = []
         self.set_name = None
-        # set the script
-        self.script = []
-        script = script.replace("(", "  ")
-        script = script.replace(")", "  ")
-        for line in script.split("\n"):
-            line = line.strip()
-            line = line.split("#")[0]
-            self.script.append(line)
-        # coroutine setup
-        self.line_number = 0
-        self.command_running = None
 
     def error(self, prompt):
         raise SnekError(
-            f"Line {self.line_number + 1} ({self.script[self.line_number]}): " + prompt
+            f"{self.lexer.error_leader('main')}{prompt}",
         )
 
-    def run(self):
-        while self.line_number < len(self.script):
-            self.cycle()
+    def _if(self, arg):
+        self.control_statements.append(bool(arg))
 
-    def cycle(self):
-        def make_token(token):
-            if not token or token[0] == "#":
-                return NoneToken(token)
-            if token == SNEKConst.EQ_OPERATOR:
-                return EQToken(token)
-            if token in self.api:
-                return CommandToken(token)
-            if token.isdigit():
-                token = ArgToken(int(token))
-                return token
-            if token[0] == token[-1] == "*":
-                return ArgToken(token[1:-1])
-            if token in self.namespace:
-                return ArgToken(self.namespace[token])
-            if token in self.kwd:
-                return KwdToken(token)
-            if token == SNEKConst.SET_COMMAND:
-                return SetToken(token)
-            if token.isidentifier():
-                return VarnameToken(token)
-            self.error(f"Unable to parse token '{token}'")
+    def _switch(self, arg):
+        self.control_statements.append(arg)
 
-        if self.command_running is not None:
-            value = next(self.command_running)
-            if value != SNEKConst.UNFINISHED:
-                if self.set_name:
-                    self.namespace[self.set_name] = value
-                self.command_running = None
-                self.line_number += 1
-        while self.command_running is None and self.line_number < len(self.script):
-            line = self.script[self.line_number]
-            tokens = [
-                make_token(token) for token in line.split("  ")
-            ]
-            tokens = [i for i in tokens if not isinstance(i, NoneToken)]
-            commands_running = min(self.conditions) and min(self.cases)
-            match tokens:
-                # empty
-                case []:
-                    pass
-                # command
-                case [CommandToken() as com, *args]:
-                    if commands_running:
-                        func = self.api[str(com)](*[arg.convert() for arg in args])
-                        value = next(func)
-                        if value == SNEKConst.UNFINISHED:
-                            self.command_running = func
-                            continue
-                        if self.set_name is not SNEKConst.NULL:
-                            self.namespace[self.set_name] = value
-                # variable setting
-                case [SetToken(), VarnameToken() as var_name]:
-                    if max(self.conditions):
-                        self._set(str(var_name))
-                case [SetToken(), VarnameToken() as var_name, ArgToken() as value] | [
-                    VarnameToken() as var_name,
-                    EQToken(),
-                    ArgToken() as value,
-                ]:
-                    if commands_running:
-                        self._set(str(var_name), value.convert())
+    def _case(self, arg):
+        self.control_statements.append(
+            arg == self.eval_arg(self.control_statements[-1])
+        )
 
-                # if endif, switch, while, etc
-                case [KwdToken() as kwd, ArgToken() as arg]:
-                    if kwd == "ENDIF" or commands_running:
-                        self.kwd[str(kwd)](arg.convert())
-                case [KwdToken() as kwd]:
-                    if kwd == "ENDIF" or commands_running:
-                        self.kwd[str(kwd)]()
-                # emtpy line
-                case []:
-                    pass
-                # here on are common errors
-                case [CommandToken(), *_]:
-                    self.error("Unexpected newline.  Perhaps you forgot 'END'?")
-                case [KwdToken(), VarnameToken() as var_name] | [
-                    CommandToken(),
-                    VarnameToken() as var_name,
-                ]:
-                    self.error(
-                        f"Unrecognized token '{var_name}'.  Perhaps you forgot to mark it as a string?"
-                    )
-                case [VarnameToken() as var_name, *_] | [ArgToken() as var_name]:
-                    self.error(f"Unrecognized command '{var_name}'.")
-                # wow you really messed up
-                case _:
-                    self.error(
-                        f"This line is so bad I can't even advise you what it is! ({tokens})"
-                    )
-            self.line_number += 1
+    def _end(self):
+        self.control_statements.pop()
 
-    def _if(self, *args):
-        if len(args) != 1:
-            self.error(f"If statement only takes one argument.  Got {args}")
-        self.conditions.append(bool(args[0]))
-
-    def _endif(self, *args):
-        if args:
-            self.error("ENDIF takes no arguments")
-        if len(self.conditions) <= 1:
-            self.error("If statement never closed")
-        self.conditions.pop(-1)
-
-    def _switch(self, *args):
-        if len(args) != 1:
-            self.error(f"Switch statement only takes one argument.  Got {args}")
-        self.switches.append(args[0])
-
-    def _endswitch(self):
-        if not self.switches:
-            self.error(f"Unexpected ENDSWITCH statement.")
-        self.switches.pop()
-
-    def _case(self, *args):
-        if len(args) != 1:
-            self.error(f"Case statement only takes one argument.  Got {args}")
-        if len(self.cases) - len(self.switches) > 1:
-            self.error(
-                f"Unexpected case statement.  Did you forget to close the first one?"
-            )
-        self.cases.append(self.switches[-1] == args[0])
-
-    def _endcase(self, *args):
-        if args:
-            self.error(f"ENDCASE takes no arguments")
-        self.cases.pop()
-
-    def _set(self, name, value=SNEKConst.NULL):
-        if value == SNEKConst.NULL:
+    def _set(self, name, value):
+        if value == NEXT:
             self.set_name = name
         else:
             self.namespace[name] = value
 
-    @staticmethod
     @snek_command
-    def print(*args):
-        print("SNEK Script:", *args)
+    def print(self, *args):
+        print(self.lexer.error_leader("main"), *args)
+
+    def run(self):
+        while self.running:
+            self.cycle()
+
+    def control_running(self):
+        if not self.control_statements:
+            return True
+        for statement in self.control_statements:
+            if not statement:
+                return False
+        return True
+
+    def get_statement(self):
+        token_list = []
+        current_token = None
+        while current_token not in {";", "{", "}"}:
+            current_token = self.lexer.get_token()
+            if current_token is self.lexer.eof:
+                if token_list:
+                    self.error("Unexpected eof while parsing" + repr(token_list))
+                self.running = False
+                return []
+            if current_token == "}" and token_list:
+                self.error("Unexpected closing bracket while parsing")
+            token_list.append(current_token)
+
+        return token_list
+
+    def is_arg(self, value):
+        if value.isidentifier():
+            return True
+        if value.isdigit():
+            return True
+        if value[0] == value[-1] and value[0] in self.lexer.quotes:
+            return True
+        if value in self.namespace:
+            return True
+        return False
+
+    def eval_arg(self, value):
+        if value == "NEXT":
+            return NEXT
+        if value in self.namespace:
+            return self.namespace[value]
+        if value.isdigit():
+            return int(value)
+        if value[0] == value[-1] and value[0] in self.lexer.quotes:
+            return value[1:-1].format(**self.namespace)
+        self.error(f"Unable to parse argument '{value}'")
+
+    def cycle(self):
+        # parsed on the fly
+        # a statement ends with a ;
+        if self.current_command is not None:
+            value = next(self.current_command)
+            if value != UNFINISHED:
+                self.current_command = None
+            if self.set_name is not None:
+                self._set(self.set_name, value)
+                self._set(self.set_name, value)
+                self.set_name = None
+        while self.current_command is None and self.running:
+            statement = self.get_statement()
+            match statement:
+                # empty line, probably eof
+                case []:
+                    pass
+                # execute a command
+                case [command, "(", *args, ")", ";"] if command in self.api:
+                    if self.control_running():
+                        args = [self.eval_arg(arg) for arg in args if arg != ","]
+                        command = self.api[command](*args)
+                        value = next(command)
+                        if value == UNFINISHED:
+                            self.current_command = command
+                            continue
+                        if self.set_name is not None:
+                            self._set(self.set_name, value)
+                            self.set_name = None
+                # command with variable assignment
+                # WHYYY does black do this
+                case [
+                    name,
+                    "=",
+                    command,
+                    "(",
+                    *args,
+                    ")",
+                    ";",
+                ] if command in self.api and name.isidentifier():
+                    if self.control_running():
+                        self.set_name = name
+                        args = [self.eval_arg(arg) for arg in args if arg != ","]
+                        command = self.api[command](*args)
+                        value = next(command)
+                        if value == UNFINISHED:
+                            self.current_command = command
+                            continue
+                        self._set(self.set_name, value)
+                        self.set_name = None
+                # control flow
+                case ["if", arg, "{"]:
+                    self._if(self.eval_arg(arg))
+                case ["switch", arg, "{"]:
+                    self._switch(arg)
+                case ["case", arg, "{"]:
+                    self._case(self.eval_arg(arg))
+                case ["}"]:
+                    self._end()
+                # variable assignment
+                case [name, "=", value, ";"] if name.isidentifier() and self.is_arg(
+                    value
+                ):
+                    self._set(name, self.eval_arg(value))
+                # errors
+                case _:
+                    self.error(f"Unable to parse line {statement}")
